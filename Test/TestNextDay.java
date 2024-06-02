e3827c7dec81c8cdc99d3726e1062bdb88759114
DODANGHOANCNTT2K15 <dodanghoana12017@gmail.com>
2024-06-03 05:41:59 +0700

    fix

--- Test/TestNextDay.java
+++ Test/TestNextDay.java
@@ -5,22 +5,32 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestNextDay {
+
     @Test
-    public void testNextDay() throws Exception {
+    public void testNextDay_EndOfMonth() {
         // Kiểm thử trường hợp ngày cuối tháng
-        NextDay nextDay1 = new NextDay(31, 1, 2023);
-        Assert.assertEquals("01/02/2023", nextDay1.getNextDay());
+        NextDay nextDay = new NextDay(31, 1, 2023);
+        Assert.assertEquals("01/02/2023", nextDay.getNextDay());
+    }
 
+    @Test
+    public void testNextDay_EndOfYear() {
         // Kiểm thử trường hợp ngày cuối năm
-        NextDay nextDay2 = new NextDay(31, 12, 2023);
-        Assert.assertEquals("01/01/2024", nextDay2.getNextDay());
+        NextDay nextDay = new NextDay(31, 12, 2023);
+        Assert.assertEquals("01/01/2024", nextDay.getNextDay());
+    }
 
+    @Test
+    public void testNextDay_LeapYear() {
         // Kiểm thử trường hợp ngày nhuận
-        NextDay nextDay3 = new NextDay(28, 2, 2024);
-        Assert.assertEquals("29/02/2024", nextDay3.getNextDay());
+        NextDay nextDay = new NextDay(28, 2, 2024);
+        Assert.assertEquals("29/02/2024", nextDay.getNextDay());
+    }
 
+    @Test
+    public void testNextDay_InvalidDate() {
         // Kiểm thử trường hợp ngày không hợp lệ
-        NextDay nextDay4 = new NextDay(31, 2, 2023);
-        Assert.assertEquals("Invalid Date", nextDay4.getNextDay());
+        NextDay nextDay = new NextDay(31, 2, 2023);
+        Assert.assertEquals("Invalid Date", nextDay.getNextDay());
     }
 }
